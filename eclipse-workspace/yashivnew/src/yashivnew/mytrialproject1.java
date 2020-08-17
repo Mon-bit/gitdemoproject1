171821dcb098882392242cb741eb6980e6fa9cec
Mon-bit <trivedimonika581@gmail.com>
2020-08-17 12:09:13 -0400

    commit to branch

--- eclipse-workspace/yashivnew/src/yashivnew/mytrialproject1.java
+++ eclipse-workspace/yashivnew/src/yashivnew/mytrialproject1.java
@@ -3,6 +3,11 @@ package yashivnew;
 public class mytrialproject1 {
 
 	public static void main(String[] args) {
+		System.out.println("hi to all");
+System.out.println("this is my first scratech project");
+
+   
+		
 		
 	}
 
